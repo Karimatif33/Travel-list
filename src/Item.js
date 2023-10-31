289d040cf7909815db1022c233692cf3180a8860
Karimatif33 <karimatif33@gmail.com>
2023-10-31 05:57:34 +0200

    finshed

--- src/Item.js
+++ src/Item.js
@@ -1,5 +1,4 @@
 
-
 export default function Item({ item, onDeleteItem, onToggleItem }) {
     
     return (
--- src/Stats.js
+++ src/Stats.js
@@ -1,4 +1,4 @@
-import Item from "./Item";
+
 
 export default function Stats({ items }) {
     if (!items.length)
